dfeffc5b5b684387b70e251e99803e359bad0bf0
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-09 20:49:33 +0300

    Update Otrezok.cpp

--- Otrezok.cpp
+++ Otrezok.cpp
@@ -5,7 +5,7 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
   float x1,x2,y1,y2;
-  cout << "Введите координаты точки A, а затем точки B" << /n;
+  cout << "Введите координаты точки A, а затем точки B" << "\n"
   cin >> x1 >> y1 >> x2 >> y2;
   x1 = x1 - x2;
   y1 = y1 - y2;
