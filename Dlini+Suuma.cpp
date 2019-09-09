a4511b26b9fc9c5ea05c440d74b5ab6d35e04387
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-09 20:57:34 +0300

    Update Dlini+Suuma.cpp

--- Dlini+Suuma.cpp
+++ Dlini+Suuma.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 #include <cmath>
+
 using namespace std;
+
 int main()
 {
-    setlocale(LC_ALL, "Russian");
-  float x1,x2,y1,y2,x3,y3;
-  cout << "Введите координаты точки A, затем точки B, а затем точки C" << /n;
-  cin >> x1 >> x2 >> x3;
-  x1 = x1 - x3;
-  x2 = x2 - x3;
-  cout << x1 << x2 << x2+x1 << "Сначала идут значения длин отрезков A и B соответственно, а затем их сумма";
-  return 0;
-}
+	setlocale(LC_ALL, "Russian");
+	float x1, x2, y1, y2, x3, y3;
+	cout << "Введите координаты точки A, затем точки B, а затем точки C" << "\n";
+	cin >> x1 >> x2 >> x3;
+	x1 = abs(x1 - x3);
+	x2 = abs(x2 - x3);
+	cout << "AC = " << x1 << " BC = " << x2 << " AC + BC = " << x2 + x1;
+	return 0;
+	}
