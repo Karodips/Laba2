b0e2c8a9b5506a91339fffad0f8ef33c95d3e922
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-09 21:05:21 +0300

    Update ProizvedenieOtrezkov.cpp

--- ProizvedenieOtrezkov.cpp
+++ ProizvedenieOtrezkov.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	float x1, x2, y1, y2, x3, y3;
+	float x1, x2, x3;
 	cout << "Введите координаты точки A, затем точки B, а затем точки C" << "\n";
 	cin >> x1 >> x2 >> x3;
 	x1 = abs(x1 - x3);
