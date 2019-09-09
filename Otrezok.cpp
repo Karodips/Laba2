419c9e5b9bd5669d24fe845b20e821add23a28c1
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-09 12:52:58 +0300

    Create Otrezok.cpp

---
+++ Otrezok.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+  float x1,x2,y1,y2;
+  cout << "Введите координаты точки A, а затем точки B" << /n;
+  cin >> x1 >> y1 >> x2 >> y2;
+  x1 = x1 - x2;
+  y1 = y1 - y2;
+  cout << sqrt(x1*x1+y1*y1);
+  return 0;
+}
