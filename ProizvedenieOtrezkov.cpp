eccd19537d95a994aeb32b86d452f7db05bb395f
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-09 21:03:18 +0300

    Create ProizvedenieOtrezkov.cpp

---
+++ ProizvedenieOtrezkov.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float x1, x2, y1, y2, x3, y3;
+	cout << "Введите координаты точки A, затем точки B, а затем точки C" << "\n";
+	cin >> x1 >> x2 >> x3;
+	x1 = abs(x1 - x3);
+	x2 = abs(x2 - x3);
+	cout << "AC * BC = " << x2 * x1;
+	return 0;
+	}
