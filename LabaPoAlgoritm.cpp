ac5494e8b8744be627c2f90b22f3d9b5a75f00a2
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-10 13:30:39 +0300

    Update LabaPoAlgoritm.cpp

--- LabaPoAlgoritm.cpp
+++ LabaPoAlgoritm.cpp
@@ -2,10 +2,10 @@
 #include <cmath>
 
 using namespace std;
-main() {
+int main() {
 	setlocale(LC_ALL, "Russian");
-	int n, stepan, factory, i;
-	double proi, kor;
+	int  factory, i, n;
+	double proi, kor, stepan;
 	stepan = 1;
 	cout << "Введите значение N \n";
 	cin >> n;
@@ -18,14 +18,26 @@ main() {
 		factory = 1;
 		proi = 1;
 	}
-	for (i = 1; i <= abs(n); i++) {
-		stepan = stepan * 2;
-		factory = factory * n;
-		proi = proi*(1 + 1 / (i*i));
-		kor = sqrt(kor) + sqrt(2);
-
+	if (n > 0) {
+		for (i = 1; i <= n; i++) {
+			stepan = stepan * 2;
+			factory = factory * i;
+			proi = proi*(1 + (1 / (i*i)));
+			kor = sqrt(kor) + sqrt(2);
+		}
 
 	}
+	else if (n < 0) {
+		for (i = 1; i >= n; i--) {
+			stepan = stepan * 2;
+			factory = factory * i;
+			proi = proi*(1 + (1 / (i*i)));
+			kor = sqrt(kor) + sqrt(2);
+
+
+		}
+	}
 	cout << "Результат 2 в степени n " << stepan << "\n" << " Результат n! " << factory << "\n" << " Результат произведений " << proi << "\n" << "Результат корней" << kor;
+	system("pause");
 	return 0;
 }
