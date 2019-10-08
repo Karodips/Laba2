e12e9772bfc9c2e1cd5b1f91dd412f6cc11187a2
Karodips <54810893+Karodips@users.noreply.github.com>
2019-10-08 13:20:57 +0300

    Create Alg43.cpp

---
+++ Alg43.cpp
@@ -0,0 +1,22 @@
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int n, i, b;
+	cout << "Сначала размер массива, затем значение b.\n";
+	cin >> n >> b;
+	int *arr = new int[n];
+	cout << "Теперь задайте массивю.\n";
+		for (i = 0; i < n; i++) {
+		cin >> arr[i];
+		if (arr[i] > b) arr[i] = 0;
+		}
+		for (i = 0; i < n; i++) cout << arr[i] << " ";
+		cout << "\n";
+	
+	system("pause");
+	return 0;
+}
